abcb3a3d9c5219d7927e4a459653faacfd82af70
Muchiachio <aldis.rameika@gmail.com>
2015-11-30 08:50:44 +0200

    Scaffolding razor attributes
    - Added support for using escaped razor attributes in templates.

--- src/Dnx.Genny/Modules/GennyTemplate.cs
+++ src/Dnx.Genny/Modules/GennyTemplate.cs
@@ -6,6 +6,7 @@ namespace Dnx.Genny
 {
     public abstract class GennyTemplate<TModel>
     {
+        private String Suffix { get; set; }
         protected TModel Model { get; set; }
         protected TextWriter Output { get; set; }
 
@@ -26,9 +27,26 @@ namespace Dnx.Genny
         {
             Output.Write(value ?? "");
         }
-        public virtual void Write(Object value)
+        public void Write(Object value)
         {
             Output.Write(value ?? "");
+        }
+
+        public void BeginWriteAttribute(String name, String prefix, Int32 prefixOffset,
+            String suffix, Int32 suffixOffset, Int32 attributeValuesCount)
+        {
+            Suffix = suffix;
+
+            Output.Write(prefix);
+        }
+        public void WriteAttributeValue(String prefix, Int32 prefixOffset, Object value,
+            Int32 valueOffset, Int32 valueLength, Boolean isLiteral)
+        {
+            Output.Write(value);
+        }
+        public void EndWriteAttribute()
+        {
+            Output.Write(Suffix);
         }
     }
 }
