0ed4d7e555b790d43d29490870b2e15271a852f9
Muchiachio <aldis.rameika@gmail.com>
2018-09-09 17:52:01 +0300

    Building the generator project
    - Added support for building the project before running the generator, to avoid running old binaries.

--- src/Genny/Program.cs
+++ src/Genny/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.DotNet.Cli.Utils;
 using NuGet.Frameworks;
 using System;
+using System.Diagnostics;
+using System.Linq;
 
 namespace Genny
 {
@@ -8,7 +10,18 @@ namespace Genny
     {
         public static void Main(String[] args)
         {
-            new GennyCommand().Execute(args);
+            if (args.Contains("--no-build") || Build())
+                new GennyCommand().Execute(args.Where(arg => arg != "--no-build").ToArray());
+        }
+
+        private static Boolean Build()
+        {
+            using (Process process = Process.Start("dotnet", "build"))
+            {
+                process.WaitForExit();
+
+                return process.ExitCode == 0;
+            }
         }
     }
 }
